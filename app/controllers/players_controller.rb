3d8a721610724a06cc1c18400ea7e1653296a42f
Cyrielle Boclet <cyrielle.boclet@gmail.com>
2017-05-30 12:30:08 +0200

    player

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,33 @@
 class PlayersController < ApplicationController
+  before_action :set_player, only: [:show, :edit, :update, :destroy ]
+
+  def index
+    @players = Player.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+    @player = Player.new()
+    @tournament = Tournament.find(params[:tournament_id])
+    @player.tournament = @tournament
+    @player.user = current_user
+    @player.save!
+  end
+
+
+  private
+
+  def set_player
+    @player = Player.find(params[:id])
+  end
+
+  def player_params
+    params.require(:player).permit(:tournament)
+  end
 end
+
--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -7,6 +7,7 @@ class TournamentsController < ApplicationController
   end
 
   def show
+    @player = Player.new()
   end
 
   def new
@@ -17,21 +18,15 @@ class TournamentsController < ApplicationController
   def create
     @tournament = Tournament.new(tournament_params)
     @tournament.user = current_user
+    # setup some params of tournament
     @tournament.bar = Bar.find(params[:bar_id])
-    if @tournament.tournament_type = "small"
-      @tournament.number_players = 8
-      @tournament.number_consoles = 2
-      @tournament.number_games = 15
-    elsif @tournament.tournament_type = "medium"
-      @tournament.number_players = 16
-      @tournament.number_consoles = 4
-      @tournament.number_games = 31
-    elsif @tournament.tournament_type = "large"
-      @tournament.number_players = 32
-      @tournament.number_consoles = 8
-      @tournament.number_games = 63
-    end
+    add_params(@tournament)
     if @tournament.save
+      @player = Player.new()
+      @player.user = current_user
+      @player.tournament = @tournament
+      @player.admin = true
+      @player.save!
       redirect_to tournament_path(@tournament)
     else
       render :new
@@ -63,5 +58,21 @@ class TournamentsController < ApplicationController
 
   def tournament_params
     params.require(:tournament).permit(:bar, :date, :status, :tournament_type, :number_players, :number_consoles, :number_games)
+  end
+
+  def add_params(tournament)
+    if tournament.tournament_type == "small"
+      tournament.number_players = 8
+      tournament.number_consoles = 2
+      tournament.number_games = 15
+    elsif tournament.tournament_type == "medium"
+      tournament.number_players = 16
+      tournament.number_consoles = 4
+      tournament.number_games = 31
+    elsif tournament.tournament_type == "large"
+      tournament.number_players = 32
+      tournament.number_consoles = 8
+      tournament.number_games = 63
+    end
   end
 end
