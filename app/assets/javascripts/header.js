e71e3cd3f692f4b575f8ee6d559381b6a4718d26
Valentin Selve <val.selve@gmail.com>
2017-06-01 17:49:32 +0200

    index 50% done

--- app/assets/javascripts/header.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $(".input-address").click(function(){
-    $("banner-toggle").removeClass("banner");
-    $("banner-toggle").addClass("banner-active");
-    $("banner-content-toggle").removeClass("banner-content");
-    $("banner-content-toggle").addClass("banner-content-active");
-  });
-});
