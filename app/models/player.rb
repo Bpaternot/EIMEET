d4b1317e9692922053707098d73f182a461648e3
arnaud boscq <arnaud@itsmycar.fr>
2017-06-02 13:04:03 +0200

    debug

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,7 @@ class Player < ApplicationRecord
   belongs_to :user
   belongs_to :tournament
   has_many :reviews, dependent: :destroy
+  #validates :ps4
+  validates :controller_ps4, presence: true
+  validates :fifa_game_ps4, presence: true
 end
