3104f55d9547904ec0866927c3b396d0c64ecd92
Valentin Selve <val.selve@gmail.com>
2017-06-03 17:08:37 +0200

    Navbar fixed

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -3,11 +3,15 @@ $(document).ready(function(){
     $(this).toggleClass("hidden");
     $(".fa-times").toggleClass("hidden");
     $(".full-screen-menu").toggleClass("hidden");
-
   });
+
   $(".fa-times").click(function(){
     $(this).toggleClass("hidden");
     $(".fa-bars").toggleClass("hidden");
     $(".full-screen-menu").toggleClass("hidden");
+  });
+
+  $(".toggle-menu").click(function(){
+    $(".toggle-menu-content").toggleClass("hidden");
   });
 });
