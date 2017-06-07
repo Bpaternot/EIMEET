9737217c2c5be9351f3a44c2859a4da0dbd8aa3c
Cyrielle Boclet <cyrielle.boclet@gmail.com>
2017-06-07 17:47:24 +0200

    un bouton par match

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,36 @@
 class GamesController < ApplicationController
+  include PoolRanking
+
   def new
   end
 
   def create
+  end
+
+  def update_scores
+    @game = Game.find(params[:id])
+    authorize(@game)
+    score_1 = @game.scores.find(params[:score_1][:id])
+    score_1.update({goals: params[:score_1][:goals]})
+    score_2 = @game.scores.find(params[:score_2][:id])
+    score_2.update({goals: params[:score_2][:goals]})
+
+
+    @tournament = @game.tournament
+    @list_all_players = generate_list_all_players(@tournament)
+
+    if @tournament.step == "group"
+      ranking_pool(@tournament)
+      @players = classify(@list_all_players)
+      @players.map! { |pool| pool.map! { |player| player.reload } }
+    end
+
+
+    respond_to do |format|
+      format.html { redirect_to tournament_playground_path(@game.tournament) }
+      format.js
+    end
+
   end
 
   private
