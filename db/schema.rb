06656eb2313ee84fadb38032156c59f531867f7e
Benoit Paternot <benoit.paternot@gmail.com>
2017-06-08 18:02:00 +0200

    Photos added, and index cards changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608080234) do
+ActiveRecord::Schema.define(version: 20170608154509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -135,6 +135,7 @@ ActiveRecord::Schema.define(version: 20170608080234) do
     t.integer  "total_goals",            default: 0
     t.integer  "total_against_goals"
     t.integer  "won_tournaments"
+    t.string   "link"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
